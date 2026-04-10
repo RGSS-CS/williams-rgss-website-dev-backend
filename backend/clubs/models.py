3888231b17cbfa11d2339ef36699efe5c8c5ba62
zhubenjamin <benjaminzhu2020@gmail.com>
2026-04-10 10:32:43 -0400

    define basic club model

--- backend/clubs/models.py
+++ backend/clubs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import Group
 
-# Create your models here.
+class Club(models.Model):
+    group = models.OneToOneField(Group, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
