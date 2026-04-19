67205b804cf4d887015099f9fe9f5fe06d4abe8c
zhubenjamin <benjaminzhu2020@gmail.com>
2026-04-18 23:06:10 -0400

    remove required calendar association w/ clubs

--- backend/calendars/models.py
+++ backend/calendars/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from clubs.models import Club
 
 class Calendar(models.Model):
-    club = models.ForeignKey(Club, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=100)
     description = models.TextField()
+
