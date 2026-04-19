a43a18cb9ca2d73147395c3f06fed61b7108f956
zhubenjamin <benjaminzhu2020@gmail.com>
2026-04-18 23:11:54 -0400

    add product id and timeone

--- backend/calendars/models.py
+++ backend/calendars/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from clubs.models import Club
+#from clubs.models import Club
 
 class Calendar(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-
+    product_id = "" # TODO: deal w/ this later
+    timezone = models.TextField("UTC") # TODO: deal w/ this later, possibly choices?
