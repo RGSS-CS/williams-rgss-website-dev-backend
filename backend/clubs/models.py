0da5038e4d67a46515640f4df69ae976bf814cf0
zhubenjamin <benjaminzhu2020@gmail.com>
2026-05-01 11:12:53 -0400

    add tag field to club

--- backend/clubs/models.py
+++ backend/clubs/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import Group
+from taggit.managers import TaggableManager
 
 class Club(models.Model):
     #group = models.OneToOneField(Group, on_delete=models.CASCADE, related_name='group')
     name = models.CharField(max_length=100)
     description = models.TextField(default="", max_length=500)
+    tags = TaggableManager()
+    
     # TODO: add other neeeded fields
 
     def __str__(self):
