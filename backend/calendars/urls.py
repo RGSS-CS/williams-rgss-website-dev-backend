5c1a9fc736e1500f67669e9d56e6e9c6301111f7
zhubenjamin <benjaminzhu2020@gmail.com>
2026-05-03 21:25:37 -0400

    clubs should use REST APIs

--- backend/calendars/urls.py
+++ backend/calendars/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path("get/ical/<int:pk>/", views.CalendarFeed(), name="calendars_calendarfeed"),
-    path("get/event/<int:pk>/", views.get_calendar_event, name="calendars_get_calendar_event")
+    path("ical/<int:pk>/", views.CalendarFeed(), name="calendars_calendarfeed"),
+    path("event/<int:pk>/", views.get_calendar_event, name="calendars_get_calendar_event")
    # path("events/", views.get_events)
 ]
--- backend/clubs/views.py
+++ backend/clubs/views.py
@@ -1,15 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
 from .models import Club
-from calendars.models import Calendar
+from rest_framework import viewsets
+from .serializers import ClubSerializer
 
-def club_info(request, *args, **kwargs):
-    club = get_object_or_404(Club, pk=kwargs["pk"])
-    club_data = {
-        "pk": club.pk,
-        "name": club.name,
-        "description": club.description,
-        "calendar": club.calendar.pk
-    }
-
-    return JsonResponse(club_data)
+class ClubViewSet(viewsets.ModelViewSet):
+    queryset = Club.objects.all()
+    serializer_class = ClubSerializer
