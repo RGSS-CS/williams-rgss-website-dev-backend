dd42d21dc4682249a2eb445aacc4ebfa73a47c3e
jerrf010 <huntingaiden513@gmail.com>
2026-05-22 14:43:16 -0400

    fix: update EventAdminForm to use a single category field and make it optional

--- backend/clubs/admin.py
+++ backend/clubs/admin.py
@@ -3,38 +3,45 @@ from django import forms
 from taggit.models import Tag
 from .models import Club, ClubGalleryImage
 
+
 class EventAdminForm(forms.ModelForm):
-    categories = forms.ModelMultipleChoiceField(
+    category = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
-        required=True,
+        required=False,
         widget=admin.widgets.FilteredSelectMultiple(
             verbose_name="Categories", is_stacked=False
         )
     )
+
     class Meta:
         model = Club
         fields = [
-            "name", "description",
+            "name", "description", "category",
             "day_of_meeting", "time", "repetition", "room_num",
             "classroom_code", "teacher_advisor"
         ]
         widgets = {
-            'time' : forms.TimeInput(format='%H:%M', attrs={'type': 'time'}),
+            'time': forms.TimeInput(format='%H:%M', attrs={'type': 'time'}),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if self.instance.pk:
-            self.fields['categories'].initial = self.instance.category.all()
-    
+        if self.instance and self.instance.pk:
+            # initial expects a list of PKs for ModelMultipleChoiceField
+            self.fields['category'].initial = [t.pk for t in self.instance.category.all()]
+
     def save(self, commit=True):
         instance = super().save(commit=False)
+        # Save instance first so TaggableManager has a PK
         if commit:
-            instance.tags.save(self.cleaned_data['tags'])
+            instance.save()
+            instance.category.set(self.cleaned_data.get('category', []))
         return instance
+
 
 @admin.register(Club)
 class EventAdmin(admin.ModelAdmin):
     form = EventAdminForm
+
 
 admin.site.register(ClubGalleryImage)
