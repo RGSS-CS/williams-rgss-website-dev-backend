8a33d38b06758305e1b4ffc9cc2504060b971181
jerrf010 <huntingaiden513@gmail.com>
2026-05-24 01:19:48 -0400

    feat: add club relation to Calendar and CalendarEvent models; update timezone default value

--- backend/calendars/models.py
+++ backend/calendars/models.py
@@ -9,10 +9,9 @@ class Calendar(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     product_id = settings.CALENDAR_PRODUCT_ID
-    timezone = models.TextField(default="UTC") # TODO: deal w/ this later, possibly choices? maybe?
+    timezone = models.TextField(default="america/toronto") # TODO: deal w/ this later, possibly choices? maybe?
     filename = models.TextField(default=f"{name}.ics")
-    #club = models.OneToOneField(Club, on_delete=models.CASCADE)
-    # ^^^ Fix issue with field
+    club = models.OneToOneField(Club, on_delete=models.CASCADE, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if self.filename and not self.filename.endswith(".ics"):
@@ -28,8 +27,7 @@ class CalendarEvent(models.Model):
     location = models.CharField(max_length=255, blank=True)
     created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(default=timezone.now)
-    #calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
-    # ^^^ Fix issue with field
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.title
