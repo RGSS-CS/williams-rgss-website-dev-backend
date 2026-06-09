de9c00e8af75ddeba5bc1bfced11b5485db724ff
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-06-08 22:49:42 -0400

    Refactor Club model fields for clarity and consistency in help text and choices

--- backend/clubs/models.py
+++ backend/clubs/models.py
@@ -25,43 +25,25 @@ class Club(models.Model):
         MONTHLY = "MONTHLY", "Monthly"
 
     class AcceptingApplications(models.TextChoices):
-        ACCEPTING = "ACCEPTING", "Accepting"
-        NOT_ACCEPTING = "NOT ACCEPTING", "Not Accepting"
-        OPEN_TO_EVERYONE = "OPEN TO EVERYONE", "Open To Everyone"
+        ACCEPTING = "ACPT", "Accepting"
+        NOT_ACCEPTING = "NACPT", "Not Accepting"
+        OPEN_TO_EVERYONE = "OpnToEvryne", "Open To Everyone"
 
     #group = models.OneToOneField(Group, on_delete=models.CASCADE, related_name='group')
     name = models.CharField(max_length=100, help_text="Insert the Name of your club")
-    preview_description = models.CharField(blank=True, 
-                                           max_length=200, 
-                                           help_text="Insert a small description for your club." \
-                                           " The long description is filled below.")
+    preview_description = models.CharField(blank=True, max_length=200, help_text="Insert a small description for your club. The long description is filled below.")
     description = models.TextField(blank=True, max_length=500)
     category = TaggableManager()
     repetition = models.CharField(blank=True, max_length=10, choices=Repetition.choices)
     image = models.ImageField(default="clubs/default.png", upload_to=get_upload_path_club)
-    classroom_code = models.CharField(blank=True, 
-                                      max_length=10, 
-                                      null=True,
-                                      help_text="This does not need an input if there is no google classroom" \
-                                      " code. *It will not be visable when selected 'Not Accepting' in the field below.")
-    accepting_applicants = models.CharField(blank=True, 
-                                           max_length=16, 
-                                           choices=AcceptingApplications.choices, 
-                                           help_text="Select 'Accepting' if applications are required. " \
-                                           "Select 'Open To Everyone' for google classroom code")
-    application_form_link = models.URLField(blank=True, 
-                                            max_length=250,
-                                            help_text="This can be either a google classroom invite link" \
-                                          " or a application form link")
-    announcement = models.CharField(null=True,
-                                    help_text="This is where you announce application news.") #BEN ISSUE
+    classroom_code = models.CharField(blank=True, max_length=10, null=True, help_text="This does not need an input if there is no google classroom code. *It will not be visable when selected 'Not Accepting' in the field below.")
+    accepting_applicants = models.CharField(blank=True, max_length=16, choices=AcceptingApplications.choices, help_text="Select 'Accepting' if applications are required. Select 'Open To Everyone' for google classroom code")
+    application_form_link = models.URLField(blank=True, max_length=250, help_text="This can be either a google classroom invite link or a application form link")
+    announcement = models.CharField(null=True, help_text="This is where you announce application news.") #BEN ISSUE
     day_of_meeting = models.CharField(max_length=10, choices=WeekDay.choices, blank=True)
     time = models.TimeField(blank=True, null=True)
     room_num = models.IntegerField(blank=True, null=True)
-    teacher_advisor = models.CharField(blank=True, 
-                                       max_length=20, 
-                                       help_text="Please insert the name of the" \
-                                       " teacher. Please insert Mr./Mrs./Ms. , followed by the last name")
+    teacher_advisor = models.CharField(blank=True, max_length=20, help_text="Please insert the name of the teacher. Please insert Mr./Mrs./Ms. , followed by the last name")
     tagline = models.CharField(blank=True, 
                                max_length=30,
                                help_text="The tagline is the title about your club. Make it intruiging such as 'A community of curious minds")
