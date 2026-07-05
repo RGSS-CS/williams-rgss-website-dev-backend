70bb8037758919fb2b4a09431e42904523b4be33
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-07-05 00:58:17 -0400

    add serializers for SocialMedia and Location models in SiteSettings

--- backend/management/serializers.py
+++ backend/management/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import SiteSettings
+from .models import SiteSettings, SocialMedia, Location
+
+class SocialMediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SocialMedia
+        fields = ["site", "content_type", "object_id"]
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ["location", "location_lat", "location_lon", "content_type", "object_id"]
 
 class SiteSettingsSerializer(serializers.ModelSerializer):
+    social_media = SocialMediaSerializer(many=True, read_only=True)
+    school_location = LocationSerializer(many=True, read_only=True)
+
     class Meta:
         model = SiteSettings
         fields = ["maintainance_mode", "site_name", "social_media", 
