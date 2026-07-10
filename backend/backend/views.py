6282b7cd3c8f1acfe5f3264d0d94edc24570f99f
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-07-10 16:32:39 +0000

    fix issues with softcode

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponse
+from django.shortcuts import redirect
 from management.models import SiteSettings
 
 def redirect_frontend(request):
     settings = SiteSettings.get_solo()
+
+    if not settings.frontend_url:
+        return HttpResponse("Frontend URL has not been configured.", status=500)
+
     return redirect(settings.frontend_url)
