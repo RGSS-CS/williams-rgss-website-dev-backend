eae4bdc03613534493af41760f1f942119d86b90
zhubenjamin <benjaminzhu2020@gmail.com>
2026-08-01 01:50:26 -0400

    create login/register views

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import UserRegisterSerializer, EmailTokenObtainPairSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class EmailTokenObtainPairView(TokenObtainPairView):
+    serializer_class = EmailTokenObtainPairSerializer
+    permission_classes = [permissions.AllowAny]
