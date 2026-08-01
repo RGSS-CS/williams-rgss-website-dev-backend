3547b2557f93cd1933904cff0a83599353007b31
zhubenjamin <benjaminzhu2020@gmail.com>
2026-08-01 01:57:34 -0400

    don't store passwords in plaintext

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -30,7 +30,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data.pop("password2")
-        user = User.objects.create(**validated_data)
+        user = User.objects.create_user(**validated_data)
         return user
 
 class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
