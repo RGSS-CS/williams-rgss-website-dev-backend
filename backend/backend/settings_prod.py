73194dca75afd08dd0d274ab879bf9d3f86d3dee
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-08-02 20:17:50 -0400

    add fallback secretkey

--- backend/backend/settings_prod.py
+++ backend/backend/settings_prod.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent # this is specifically only for use in this file
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "build-fallback-secret-key")
 DEBUG = False
 ALLOWED_HOSTS = [
     h.strip() for h in os.environ.get("ALLOWED_HOSTS", "").split(",") if h.strip()
