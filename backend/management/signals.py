4a4f5165ebc2c1ea07666f90a3a8a1f13c585be3
jerrf010 <huntingaiden513@gmail.com>
2026-08-04 19:54:46 -0400

    add defaults to page settings

--- backend/management/signals.py
+++ backend/management/signals.py
@@ -4,28 +4,28 @@ def on_post_migrate(sender, app_config, *args, **kwargs):
     if not PageSettings.objects.filter(internal_site_name="HM").exists():
         PageSettings.objects.create(
             internal_site_name="HM",
-            title="School Home",
-            subtitle="",
-            tagline="",
+            title="Welcome To",
+            subtitle="STUCO",
+            tagline="Where students come together",
         )
     if not PageSettings.objects.filter(internal_site_name="CL").exists():
         PageSettings.objects.create(
             internal_site_name="CL",
-            title="Clubs",
-            subtitle="",
-            tagline="",
+            title="Find Your",
+            subtitle="Club",
+            tagline="A million ways to get involved, one club at a time",
         )
     if not PageSettings.objects.filter(internal_site_name="GL").exists():
         PageSettings.objects.create(
             internal_site_name="GL",
             title="Gallery",
             subtitle="",
-            tagline="",
+            tagline="Capturing the moments that matter most",
         )
     if not PageSettings.objects.filter(internal_site_name="AB").exists():
         PageSettings.objects.create(
             internal_site_name="AB",
-            title="About School",
+            title="About STUCO",
             subtitle="",
-            tagline="",
+            tagline="What about it? It's the student council...",
         )
