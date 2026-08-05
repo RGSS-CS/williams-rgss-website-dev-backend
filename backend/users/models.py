0090a4d89dbd115dc6f6eb8e6805732fea107340
jerrf010 <huntingaiden513@gmail.com>
2026-08-05 11:08:32 -0400

    edit fields and add migrations

--- backend/users/models.py
+++ backend/users/models.py
@@ -6,9 +6,9 @@ def get_random_code():
     return get_random_string(length=32)
 
 class UserJoinCode(models.Model):
-    code = models.CharField(default=get_random_code, unique=True)
-    label = models.CharField()
-    description = models.TextField(blank=True)
+    code = models.CharField(default=get_random_code, unique=True, verbose_name="Security Code")
+    label = models.CharField(max_length=50, blank=True, verbose_name="Title", help_text="A easily identifiable name for this qr code, e.g. 'Cafeteria'")
+    description = models.TextField(blank=True, max_length=300)
     expiry = models.DateTimeField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
