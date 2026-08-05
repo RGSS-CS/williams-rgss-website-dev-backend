e92a61b68f7fe8e87f0024e2029e475342b52c12
jerrf010 <huntingaiden513@gmail.com>
2026-08-05 11:20:58 -0400

    change datetime widget

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django import forms
 from .models import UserJoinCode
+from django.contrib.admin import widgets
 
 class UserJoinCodeForm(forms.ModelForm):
     class Meta:
@@ -16,6 +17,7 @@ class UserJoinCodeForm(forms.ModelForm):
         widgets = {
             'label': forms.TextInput(attrs={'size': 40}),
             'description': forms.Textarea(attrs={'rows': 4, 'cols': 60}),
+            'expiry': widgets.AdminSplitDateTime(attrs={'type': 'datetime-local'}),
         }
 
 @admin.register(UserJoinCode)
