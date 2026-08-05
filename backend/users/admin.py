a3a55ce1bbfb93a094c5abf51345ca7627e8cf6f
jerrf010 <huntingaiden513@gmail.com>
2026-08-05 11:02:36 -0400

    add admin form for customization

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
+from django import forms
 from .models import UserJoinCode
+
+class UserJoinCodeForm(forms.ModelForm):
+    class Meta:
+        model = UserJoinCode
+        fields = [
+            'enabled',
+            'label',
+            'description',
+            'expiry',
+            'max_uses',
+            'uses',
+        ]
+        widgets = {
+            'label': forms.TextInput(attrs={'size': 40}),
+            'description': forms.Textarea(attrs={'rows': 4, 'cols': 60}),
+        }
 
 @admin.register(UserJoinCode)
 class UserJoinCodeAdmin(admin.ModelAdmin):
+    form = UserJoinCodeForm
     readonly_fields = ('code', 'uses', 'created', 'updated')
