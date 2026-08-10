a6ff96d257891777ba95b3c324b3b4033a40653f
jerrf010 <huntingaiden513@gmail.com>
2026-08-09 22:20:31 -0400

    remove duplicate whitenoise middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
