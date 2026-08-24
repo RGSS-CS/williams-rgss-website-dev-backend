3757b0cec63c328f78396d0f3940f6557277db2c
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-08-23 21:56:56 -0400

    add email field

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django import forms
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import UserJoinCode
 from django.contrib.admin import widgets
 
@@ -24,3 +26,14 @@ class UserJoinCodeForm(forms.ModelForm):
 class UserJoinCodeAdmin(admin.ModelAdmin):
     form = UserJoinCodeForm
     readonly_fields = ('code', 'uses', 'created', 'updated')
+
+class UserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
