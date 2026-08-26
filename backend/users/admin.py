caa1b8cde4a495b8a52faac734a87a70804a741c
zhubenjamin <benjaminzhu2020@gmail.com>
2026-08-26 12:29:38 -0700

    display code as qr code in admin panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django import forms
 from .models import UserJoinCode
 from django.contrib.admin import widgets
+from django.utils.html import format_html
+import qrcode
+import io
+import base64
 
 class UserJoinCodeForm(forms.ModelForm):
     class Meta:
@@ -23,4 +27,20 @@ class UserJoinCodeForm(forms.ModelForm):
 @admin.register(UserJoinCode)
 class UserJoinCodeAdmin(admin.ModelAdmin):
     form = UserJoinCodeForm
-    readonly_fields = ('code', 'uses', 'created', 'updated')
+    readonly_fields = ('code', 'code_preview', 'uses', 'created', 'updated')
+
+    def code_preview(self, obj) -> str:
+        if obj.code:
+            url = f"{obj.code}"
+            qr = qrcode.make(url)
+            buffer = io.BytesIO()
+            qr.save(buffer, format="PNG")
+
+            qr_b64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+            return format_html(
+                '<img src="data:image/png;base64,{}" style="max-height: 200px;" />',
+                qr_b64
+            )
+        else:
+            return "Error: it seems that the code field is null, or in Python, None. This shouldn't have happened."
