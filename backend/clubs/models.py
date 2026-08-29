ad6f8be53d887d4d614e5fd39a27c889107dc032
David <d4nplus6@gmail.com>
2026-08-28 21:20:02 -0400

    Formatted clubs models for better readability

--- backend/clubs/models.py
+++ backend/clubs/models.py
@@ -51,25 +51,56 @@ class Club(models.Model):
         help_text="Insert a long description for your club. This is where you can describe your club in detail."
     )
     category = TaggableManager()
-    repetition = models.CharField(null=True, max_length=10, choices=Repetition.choices, help_text="How often does your club meet? If your club meets on a different schedule, please select 'Weekly' and specify in the description.")
-    classroom_code = models.CharField(max_length=10, null=True, help_text="This does not need an input if there is no google classroom code. *It will not be visable when selected 'Not Accepting' in the field below.")
-    accepting_applicants = models.CharField(null=True, max_length=16, choices=AcceptingApplications.choices, help_text="Select 'Accepting' if applications are required. Select 'Open To Everyone' for google classroom code")
-    application_form_link = models.URLField(blank=True, null=True, max_length=250, help_text="This can be either a google classroom invite link or a application form link *It will not be visable when selected 'Not Accepting' in the field below.")
-    announcement = models.CharField(null=True, help_text="This is where you announce application news.") #BEN ISSUE
+    repetition = models.CharField(
+        null=True, max_length=10, choices=Repetition.choices, 
+        help_text="How often does your club meet? If your club meets on a different schedule," \
+        " please select 'Weekly' and specify in the description."
+    )
+    classroom_code = models.CharField(
+        max_length=10, null=True, 
+        help_text="This does not need an input if there is no google classroom code. "
+        "*It will not be visable when selected 'Not Accepting' in the field below."
+    )
+    accepting_applicants = models.CharField(
+        null=True, max_length=16, choices=AcceptingApplications.choices, 
+        help_text="Select 'Accepting' if applications are required. Select 'Open To Everyone' for google classroom code"
+    )
+    application_form_link = models.URLField(
+        blank=True, null=True, max_length=250, 
+        help_text="This can be either a google classroom invite link or a application" \
+        " form link *It will not be visable when selected 'Not Accepting' in the field below."
+    )
+    announcement = models.CharField(
+        null=True, help_text="This is where you announce application news."  #BEN ISSUE
+    )
     day_of_meeting = models.CharField(max_length=10, choices=WeekDay.choices, null=True)
     time = models.TimeField(null=True)
     room_number = models.PositiveIntegerField(null=True)
-    teacher_advisor = models.CharField(max_length=20, help_text="Please insert the name of the teacher. Please insert Mr./Mrs./Ms. , followed by the last name")
-    tagline = models.CharField(blank=True, null=True, max_length=30, help_text="The tagline is the title about your club. Make it intruiging such as 'A community of curious minds'")
-    join_instructions = models.TextField(default="Use the google classroom code or application form link to join.", max_length=500, help_text="This is where you tell the students how to join, such as using a google classroom code or a link to a form. *It will not be visable when selected 'Not Accepting' in the field below.")
+    teacher_advisor = models.CharField(
+        max_length=20, help_text="Please insert the name of the teacher. " \
+        "Please insert Mr./Mrs./Ms. , followed by the last name"
+    )
+    tagline = models.CharField(
+        blank=True, null=True, max_length=30, 
+        help_text="The tagline is the title about your club. Make it intruiging such as" \
+        " 'A community of curious minds'"
+    )
+    join_instructions = models.TextField(
+        default="Use the google classroom code or application form link to join.", max_length=500, 
+        help_text="This is where you tell the students how to join, such as " \
+        "using a google classroom code or a link to a form. *It will not be " \
+        "visible when selected 'Not Accepting' in the field below."
+    )
     social_media = GenericRelation(SocialMedia)
-    gallery = models.ForeignKey(Gallery, on_delete=models.SET_NULL, null=True, blank=True, related_name="clubs",)
+    gallery = models.ForeignKey(
+        Gallery, on_delete=models.SET_NULL, null=True, blank=True, related_name="clubs"
+    )
 
     PENDING_APPROVAL_FIELDS = [
         "name", "preview_description", "description", "tagline",
         "repetition", "classroom_code", "accepting_applicants",
         "application_form_link", "announcement", "day_of_meeting", "time",
-        "room_number", "teacher_advisor", "join_instructions",
+        "room_number", "teacher_advisor", "join_instructions"
     ]
 
     def __str__(self):
@@ -141,7 +172,10 @@ class ClubMembership(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='club_memberships')
     club = models.ForeignKey(Club, on_delete=models.CASCADE, related_name="memberships")
     role = models.CharField(max_length=20, choices=Role.choices, default=Role.EXECUTIVE)
-    bypass_confirmation_restrictions = models.BooleanField(default=False, help_text="This allows execs to publish changes without approval by the club administrator")
+    bypass_confirmation_restrictions = models.BooleanField(
+        default=False, help_text="This allows execs to publish " \
+        "changes without approval by the club administrator"
+    )
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -172,9 +206,15 @@ class ClubChanges(models.Model):
     club = models.ForeignKey(Club, on_delete=models.CASCADE, related_name='pending_edits')
     changes = models.JSONField(help_text="Changed Value")
     status = models.CharField(max_length=10, choices=ApprovalStatus.choices, default= ApprovalStatus.PENDING)
-    submitted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='submitted_changes')
+    submitted_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, 
+        null=True, related_name='submitted_changes'
+    )
     submitted_at = models.DateTimeField(auto_now_add=True)
-    reviewed_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, default ="Pending Reviewer", related_name='reviewed_club_changes')
+    reviewed_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, 
+        default ="Pending Reviewer", related_name='reviewed_club_changes'
+    )
     reviewed_at = models.DateTimeField(null=True, blank = True)
     review_note = models.TextField(max_length=200, blank=True)
 
