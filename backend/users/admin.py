fc5dd301db8b781dc55a1e3411ebe526fae9113e
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-09-01 14:57:24 -0400

    remove title field

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -57,11 +57,11 @@ class UserJoinCodeAdmin(admin.ModelAdmin):
             return "Error: it seems that the code field is null, or in Python, None. This shouldn't have happened."
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'title')
-    search_fields = ('username', 'email', 'first_name', 'last_name', 'title')
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'title')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
@@ -69,7 +69,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'first_name', 'last_name', 'title', 'password1', 'password2'),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2'),
         }),
     )
 
