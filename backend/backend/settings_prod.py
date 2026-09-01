7d61e8018c494eae1d8e03ba5be13fd1c3806248
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-09-01 12:03:07 -0400

    add debug configuration to troubleshooting

--- backend/backend/settings_prod.py
+++ backend/backend/settings_prod.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent # this is specifically only for use in this file
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "build-fallback-secret-key")
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False")
 ALLOWED_HOSTS = [
     h.strip() for h in os.environ.get("ALLOWED_HOSTS", "").split(",") if h.strip()
 ]
