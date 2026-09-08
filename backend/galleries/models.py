4e4be33412d52c8ca8f8a7fa0578784bd988fe7b
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-09-07 23:08:21 -0400

    fix intendation

--- backend/galleries/models.py
+++ backend/galleries/models.py
@@ -30,7 +30,7 @@ class Photos(models.Model):
         return self.name
 
     def save(self, *args, **kwargs):
-         if not self.name or not self.name.strip:
+        if not self.name or not self.name.strip:
             club_name = self.club.name
             self.name = f'{club_name[:17]} - {get_random_string(8)}'
             super().save(*args, **kwargs)
