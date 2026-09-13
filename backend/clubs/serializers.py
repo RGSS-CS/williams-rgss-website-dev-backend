db2b730f50a2586365f7aed0c4a6b3c7c7bb6038
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-09-13 11:42:04 -0400

    fix error

--- backend/clubs/serializers.py
+++ backend/clubs/serializers.py
@@ -22,7 +22,7 @@ class ClubSerializer(TaggitSerializer, serializers.ModelSerializer):
         fields = [
             "id", "name", "preview_description", "description", 
             "tagline", "category", "day_of_meeting", "time", 
-            "repetition", "room_number", "why_join", "classroom_code", 
+            "repetition", "location", "why_join", "classroom_code", 
             "accepting_applicants", "join_instructions", "application_form_link", 
             "teacher_advisor", "announcement"
             ]
