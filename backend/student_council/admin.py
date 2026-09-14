a030ce6668a73aacf1a3161e998a70646dd75a73
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-09-13 22:59:00 -0400

    get some stuff going

--- backend/student_council/admin.py
+++ backend/student_council/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import STUCO, Ticker
+from solo.admin import SingletonModelAdmin
+from django import forms
+from django.contrib.admin import widgets
 
-# Register your models here.
+class STUCOAdminForm(forms.ModelForm):
+    class Meta:
+        model = STUCO
+        fields = [
+            'council_name', 'group_photo'
+        ]
+    
+
+@admin.register(STUCO)
+class STUCOAdmin(SingletonModelAdmin):
+    form = STUCOAdminForm
+
+admin.site.register(Ticker)
--- backend/student_council/apps.py
+++ backend/student_council/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class StudentCouncilConfig(AppConfig):
     name = 'student_council'
+    verbose_name = 'Student Council'
