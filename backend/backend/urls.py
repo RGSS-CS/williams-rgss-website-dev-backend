fc6a8655cae90af809145ee79d359ec748a60b0f
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-09-14 21:08:35 -0400

    make seralizers

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -37,4 +37,5 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("api/management/", include("management.urls")),
+    path('api/stuco/', include('student_council.urls'))
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/student_council/views.py
+++ backend/student_council/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import STUCOSeralizer, AnnouncementSeralizer
+from .models import STUCO, Announcements 
 
-# Create your views here.
+class STUCOViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = STUCO.objects.all()
+    serializer_class = STUCOSeralizer
+
+class AnnouncementViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Announcements.objects.all()
+    serializer_class = AnnouncementSeralizer
