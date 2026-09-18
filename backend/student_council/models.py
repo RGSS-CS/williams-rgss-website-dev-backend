b775323d3436d630c293ebed282479fe65838e78
jerrf010 <huntingaiden513@gmail.com>
2026-09-18 13:58:59 +0000

    Add new image field to homepage; set maintenance mode default to true

--- backend/student_council/models.py
+++ backend/student_council/models.py
@@ -26,7 +26,8 @@ class STUCO(SingletonModel):
     council_name = models.CharField(default="STUCO", max_length=10, help_text="The name of the council (e.g, SAC)")
     group_photo = models.ImageField(blank=True, null=True, upload_to='upload/stuco/')
     photo_caption = models.TextField(blank=True, null=True, help_text='A legend for the people present in the photo.')
-
+    stuco_logo = models.ImageField(blank=True, null=True, help_text='The logo that will appear on the homepage logo section.')
+    
     class Meta: 
         verbose_name = 'Student Council Settings'
         verbose_name_plural = 'Student Council Settings'
